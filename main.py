eaa5d77399a78bdfa0073dcc63f310cf5c434539
martinbrg <martin.brg@gmail.com>
2025-01-08 08:54:35 +0100

    Added first sprites, player class

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     #getting a new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #the next 2 are for adjusting the FPS rate
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
+
+    #new player object
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
+
     while True:
         #this will help actually quitting the infinite loop:
         for event in pygame.event.get():
@@ -13,10 +22,11 @@ def main():
                 return
 
         screen.fill("black")
-        #thats the refresh:
+        #thats the refresh, for the sprites and afterwards for the entire screen:
+        player.draw(screen, "white")
         pygame.display.flip()
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(1)/1000
+        #elihw ...end of game loop
 
 
 
---
+++ player.py
@@ -0,0 +1,22 @@
+from circleshape import *
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen, color):
+        #last param is line width, constant 2
+        pygame.draw.polygon(screen, color, self.triangle(), 2)
+
+
+        return super().draw(screen)
